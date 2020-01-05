02bda6482d79999e2382bcec925490a65655b563
Stephanie <sbowl001@fiu.edu>
2020-01-06 01:27:36 +0800

    controller draft

--- Pokemon Objc/Pokemon Objc/PokemonController.swift
+++ Pokemon Objc/Pokemon Objc/PokemonController.swift
@@ -10,4 +10,78 @@ import UIKit
 
 @objc (PokemonController)
 class PokemonController: NSObject {
+    
+    let baseURL = URL(string:"")!
+    
+    @objc var pokemons: [SBOPokemon] = []
+    
+    @objc static let shared = PokemonController()
+    
+    
+    @objc func getPokemon(completion: @escaping ([SBOPokemon?], Error?) -> Void) {
+        let requestURL = baseURL
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            if let error = error {
+                NSLog("error getting Pokemon \(error)")
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else { NSLog("error returning data")
+                completion(nil, nil)
+                return
+      
+                }
+            do {
+            guard let dictionary = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {return}
+            let pokemonDictionaries = dictionary["results"] as! [[String: Any]]
+            for pokemonDictionary in pokemonDictionaries {
+                let pokemons = SBOPokemon(dictionary: pokemonDictionary)
+                self.pokemons.append(pokemon)
+            }
+            completion(self.pokemons, nil)
+        }
+            catch {
+                    NSLog("Error decoding random users JSON \(error)")
+                                   completion(nil, error)
+                                   return
+                }
+            }.resume()
+
+    }
+ @objc func fillInPerson(person: SBOPokemon) {
+    let randomIndex = Int(arc4random_uniform(999));
+    let requestURL = baseURL
+    URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+        if let error = error {
+            NSLog("Error getting Users \(error)")
+            return
+        }
+        guard let data = data else {
+            NSLog("Error returning data")
+            return
+        }
+         do {
+                guard let dictionary = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {return}
+                let pokemonDictionaries = dictionary["name"] as! [[String: Any]]
+                let pokemonDictionary = pokemonDictionaries[randomIndex]
+                let identifier = pokemonDictionary["identifier"] as! Identifier
+                let abilities = pokemonDictionary["abilities"]["ability"]["name"] as! String
+                let pictureObject = pokemonDictionary["sprites"] as! [String: Any]
+                let largeImageString = pictureObject["front_default"] as! String
+                pokemon.identifier = identifier
+              pokemon.abilities = abilities
+                let largeImageData = try! Data(contentsOf: URL(string: largeImageString)!)
+                
+                let image = UIImage(data: largeImageData)!
+                pokemon.sprite = image
+                  
+            } catch {
+                NSLog("error decoding random users JSON \(error)")
+                    return
+                }
+                
+            }.resume()
+        
+}
 }
--- Pokemon Objc/Pokemon Objc/SBOPokemon.m
+++ Pokemon Objc/Pokemon Objc/SBOPokemon.m
@@ -7,6 +7,7 @@
 //
 
 #import "SBOPokemon.h"
+#import <UIKit/UIKit.h> 
 
 @implementation SBOPokemon
 
